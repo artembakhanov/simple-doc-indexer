e014f8891dc6d7a017da9fa520a4d69c552b6618
Artem Bakhanov <artembakhanov@gmail.com>
2020-09-17 18:48:51 +0300

    Add lib for query

---
+++ src/main/java/lib/Vocabulary.java
@@ -0,0 +1,33 @@
+package lib;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class Vocabulary {
+
+    public static HashMap<String, Word> loadVocabulary(Configuration conf, String path) throws IOException {
+        HashMap<String, Word> words = new HashMap<>();
+        FileSystem fileSystem = FileSystem.get(conf);
+        FileStatus[] files = fileSystem.listStatus(new Path(path + "/final"),
+                path1 -> path1.getName().startsWith("part"));
+
+        for (FileStatus file : files) {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(fileSystem.open(file.getPath())));
+
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                String[] params = line.split("\t");
+                words.put(params[0], new Word(params));
+            }
+        }
+
+        return words;
+    }
+}
---
+++ src/main/java/lib/Word.java
@@ -0,0 +1,43 @@
+package lib;
+
+public class Word {
+    private String word;
+    private int id;
+    private double idf;
+
+    public Word(String word, int id, int idf) {
+        this.word = word;
+        this.id = id;
+        this.idf = idf;
+    }
+
+    public Word(String[] params) {
+        this.word = params[0];
+        this.idf = Integer.parseInt(params[1]);
+        this.id = Integer.parseInt(params[2]);
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getIdf() {
+        return idf;
+    }
+
+    public void setIdf(double idf) {
+        this.idf = idf;
+    }
+}
